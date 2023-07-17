a7eb008aa5ca89957a4be096517639cc691a2f76
damix69420 <76411806+damix69420@users.noreply.github.com>
2023-07-17 22:22:55 +0200

    get geo data

---
+++ src/utils/location.ts
@@ -0,0 +1,12 @@
+export async function getIPLocation(ip: string) {
+    const result = await fetch(`https://get.geojs.io/v1/ip/geo/${ip}.json`);
+    const data = await result.json();
+
+    return data;
+}
+
+export async function getGeoData(lat: number, lon: number) {
+    const result = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`);
+    const data = await result.json();
+    return data;
+}
