e6e5915a5585485e99cb48d6886a6af99323c105
Damjan Pavličević <damix4921@gmail.com>
2024-02-19 19:22:15 +0100

    fix an issue where it would load before GPS initialized, also when you click on the home icon it refreshes

--- src/socket/socket_server.ts
+++ src/socket/socket_server.ts
@@ -6,6 +6,8 @@ import authorizeSocket from "./authorization";
 let io: Server;
 
 export async function initSocketServer(server: any) {
+    // Create the table if it doesn't exist
+    // This is for making load balancing work with socket.io using the Postgres adapter
     await executeQuery(`CREATE TABLE IF NOT EXISTS socket_io_attachments (
         id          bigserial UNIQUE,
         created_at  timestamptz DEFAULT NOW(),
@@ -21,25 +23,28 @@ export async function initSocketServer(server: any) {
         const headers = socket.handshake.headers;
 
         if (!headers.authorization) {
+            // Disconnect the socket if the authorization header is not present
             socket.disconnect(true);
         }
 
-        const user = await authorizeSocket(headers.authorization!);
+        const user = await authorizeSocket(headers.authorization!); // Authorize the user
 
         if (!user) {
-            socket.disconnect(true);
+            socket.disconnect(true); // Disconnect the socket if the user is not authorized
             return;
         }
 
         console.log(`User ${user!.username} connected`);
-        socket.join(user!.uuid);
+        socket.join(user!.uuid); // Join the room with the user's UUID
 
         socket.on("disconnect", () => {
-            console.log("Socket disconnected");
+            console.log(
+                `Socket disconnected from user ${user?.username || "unknown"}`
+            );
         });
     });
 }
 
 export function emitToUser(uuid: string, event: string, ...args: any[]) {
-    io.to(uuid).emit(event, ...args);
+    io.to(uuid).emit(event, ...args); // Emit the event to the user's room
 }
